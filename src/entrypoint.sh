36fdf2e6d6b5a4b1dcf8fb756d27ac4d35966f33
Meelis Valgevali <meelis@fenomen.ee>
2016-04-26 16:11:21 +0300

    Executes php wrong permission

--- src/entrypoint.sh
+++ src/entrypoint.sh
@@ -1,10 +1,11 @@
 #!/usr/bin/env bash
+c	 
 
 case "${1}" in
   build)
     /bin/su - root -mc "apt-get update && /src/php-4.4/build.sh && /src/php-4.4/clean.sh"
     ;;
   run)
-    /bin/su - root -mc "source /src/php-4.4/variables.sh && /src/php-4.4/run.sh"
+    /bin/su - www-data -mc "source /src/php-4.4/variables.sh && /src/php-4.4/run.sh"
     ;;
 esac
