6659ec80dd6c36bf1c1cbdb1d01cd40c6d27d183
Meelis Valgevali <meelis@fenomen.ee>
2016-04-27 10:59:53 +0300

    Permissions

--- src/entrypoint.sh
+++ src/entrypoint.sh
@@ -5,6 +5,6 @@ case "${1}" in
     /bin/su - root -mc "apt-get update && /src/php-4.4/build.sh && /src/php-4.4/clean.sh"
     ;;
   run)
-    /bin/su - root -mc "source /src/php-4.4/variables.sh && /src/php-4.4/run.sh"
+    /bin/su - root -mc "source /src/php-4.4/variables.sh && /bin/su - www-data -mc \"/src/php-4.4/run.sh\""
     ;;
 esac
